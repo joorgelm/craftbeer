a1b15767c1654987d8ea28aa1ad0d153a1be4830
jorge.melgarejo <jorge.melgarejo@azi.com.br>
2021-03-18 20:31:18 -0400

    [FEATURE#001] - adiciona sequencia no script inicial

--- init.sql
+++ init.sql
@@ -1,13 +1,19 @@
-CREATE DATABASE craftbeer;
+CREATE SCHEMA craftbeer;
 
-USE craftbeer;
+CREATE SEQUENCE "craftbeer"."seq_tb_beer"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
-CREATE TABLE tb_beer (
-    beer_id SERIAL PRIMARY KEY,
-    beer_name VARCHAR(100) NOT NULL,
-    beer_ingredients VARCHAR(500) NOT NULL,
-    beer_alcoholContent VARCHAR(255) NOT NULL,
-    beer_price NUMERIC(18, 8) NOT NULL
+CREATE TABLE "craftbeer".tb_beer (
+                         beer_id INTEGER PRIMARY KEY,
+                         beer_name VARCHAR(100) NOT NULL,
+                         beer_ingredients VARCHAR(500) NOT NULL,
+                         beer_alcohol_content VARCHAR(255) NOT NULL,
+                         beer_category VARCHAR(255) NOT NULL,
+                         beer_price NUMERIC(18, 8) NOT NULL
 );
 
 COMMENT ON TABLE "craftbeer"."tb_beer" IS 'Tabela contendo as cervejas.';
@@ -18,11 +24,13 @@ COMMENT ON COLUMN "craftbeer"."tb_beer"."beer_name" IS 'Nome da cerveja.';
 
 COMMENT ON COLUMN "craftbeer"."tb_beer"."beer_ingredients" IS 'Ingredientes utilizados na fabricação.';
 
-COMMENT ON COLUMN "craftbeer"."tb_beer"."beer_alcoholContent" IS 'Teor alcoolico.';
+COMMENT ON COLUMN "craftbeer"."tb_beer"."beer_alcohol_content" IS 'Teor alcoolico.';
+
+COMMENT ON COLUMN "craftbeer"."tb_beer"."beer_category" IS 'Categoria da cerveja.';
 
 COMMENT ON COLUMN "craftbeer"."tb_beer"."beer_price" IS 'Valor de venda.';
 
-INSERT INTO tb_beer(beer_id, beer_name, beer_ingredients, beer_alcoholContent, beer_price)
-VALUES  (1, 'Red Ale', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id varius est.', '14%', 14.5)
-        (2, 'Americam Ale', 'Lorem consectetur adipiscing elit.', '10%', 10.5)
-        (3, 'IPA das Braba', 'Lorem consectetur adipiscing elit.', '20%', 17.5);
+INSERT INTO "craftbeer".tb_beer(beer_id, beer_name, beer_ingredients, beer_alcohol_content, beer_category, beer_price)
+VALUES  (nextval('seq_tb_beer'), 'Red Ale', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id varius est.', '14%', 'RED ALE', 14.5),
+        (nextval('seq_tb_beer'), 'Americam Ale', 'Lorem consectetur adipiscing elit.', '10%', 'AMERICAN', 10.5),
+        (nextval('seq_tb_beer'), 'IPA das Braba', 'Lorem consectetur adipiscing elit.', '20%', 'IPA', 17.5);
